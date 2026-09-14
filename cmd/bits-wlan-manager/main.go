45605c24a20820ee2ec66c6ff03011ed7298cd6c
obliviousorion <obliviousorion@gmail.com>
2026-09-14 12:51:35 +0530

    test(cmd): trigger engine Tick in main entrypoint

--- cmd/bits-wlan-manager/main.go
+++ cmd/bits-wlan-manager/main.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 	client := auth.NewClient()
-	eng := engine.New(client, "testuser", "testpass")
+	eng := engine.New(client, "F20230814", "F20237057#")
 
 	fmt.Printf("[TEST] Initial State: %s\n", eng.State())
+
+	eng.Tick()
+
+	fmt.Printf("[TEST] Final State: %s\n", eng.State())
 }
